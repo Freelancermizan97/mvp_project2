71b7625d2187ee08d9b9430f9c7d7222a3208bd3
Freelancermizan97 <md.mizanhossan9990@gmail.com>
2022-08-27 21:39:05 +0600

    dome

--- lib/post/post_page.dart
+++ lib/post/post_page.dart
@@ -159,45 +159,60 @@ class _PostPageState extends State<PostPage>
               ),
             ),
             Spacer(),
-            Container(
-              padding: EdgeInsets.only(left: 10.r, right: 10.r),
-              height: 90.h,
-              color: Color(0xffF9F9F9),
-              child: Row(
-                children: [
-                  Spacer(),
-                  Container(
-                    height: 50.h,
-                    width: 350 .w,
-                    padding: EdgeInsets.only(left: 20.r),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Color(0xffE5E5EA)
-                      ),
-                      borderRadius: BorderRadius.circular(15.r),
-                      color: Color(0xffFFFFFF),
-                    ),
-                    child:  TextField(
-
-                      decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Write a comment",
-                          hintStyle: TextStyle(fontFamily: 'Nun', fontSize: 17, color: Color(0xff979797))
-                      ),
-                    ),
-                  ),
-                  Spacer(),
-                  Image.asset("assets/icon/send_icon.png", height: 30.h,),
-                  Spacer(),
-                ],
-              ),
-            )
+            WriteComments()
           ],
         ),
       ),
     );
   }
 }
+
+class WriteComments extends StatefulWidget {
+  const WriteComments({Key? key}) : super(key: key);
+
+  @override
+  State<WriteComments> createState() => _WriteCommentsState();
+}
+
+class _WriteCommentsState extends State<WriteComments> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10.r, right: 10.r),
+      height: 90.h,
+      color: Color(0xffF9F9F9),
+      child: Row(
+        children: [
+          Spacer(),
+          Container(
+            height: 50.h,
+            width: 350 .w,
+            padding: EdgeInsets.only(left: 20.r),
+            decoration: BoxDecoration(
+              border: Border.all(
+                  color: Color(0xffE5E5EA)
+              ),
+              borderRadius: BorderRadius.circular(15.r),
+              color: Color(0xffFFFFFF),
+            ),
+            child:  TextField(
+
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: "Write a comment",
+                  hintStyle: TextStyle(fontFamily: 'Nun', fontSize: 17, color: Color(0xff979797))
+              ),
+            ),
+          ),
+          Spacer(),
+          Image.asset("assets/icon/send_icon.png", height: 30.h,),
+          Spacer(),
+        ],
+      ),
+    );
+  }
+}
+
 
 
 
